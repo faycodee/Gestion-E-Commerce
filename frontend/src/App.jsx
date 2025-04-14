571c0b21d083e50e6e7a984c2deb0a469ba597a4
Faycal <faysaloumzil1@gmail.com>
2025-04-14 12:29:49 +0100

    Faycal:remove backend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,14 +10,14 @@ import Highlights from "./components/highlights";
 import Footer from "./components/Footer";
 import Shop from "./components/Shop";
 import SingleProduct from "./components/SingleProduct";
-import Categories from "./components/Categories";
-import Contact from "./components/Contact";
-<<<<<<< HEAD
+// import Categories from "./components/Categories";
+// import Contact from "./components/Contact";
+// <<<<<<< HEAD
 import Login from "./components/Login";
 import Signup from "./components/Signup";
-=======
-import Faq from "./components/FAQ";
->>>>>>> 7703476b91e6fd48f497c7ca051b283e6e87765f
+// =======
+// import Faq from "./components/FAQ";
+// >>>>>>> 7703476b91e6fd48f497c7ca051b283e6e87765f
 
 const App = () => (
   <BrowserRouter>
@@ -40,10 +40,10 @@ const App = () => (
         />
         <Route path="/shop" element={<Shop />} />
         <Route path="/product/:id" element={<SingleProduct />} />
-        <Route path="/categories" element={<Categories />} />
-        <Route path="/faq" element={<Faq />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/contact" element={<Contact />} />
+        {/* <Route path="/categories" element={<Categories />} /> */}
+        {/* <Route path="/faq" element={<Faq />} /> */}
+        {/* <Route path="/contact" element={<Contact />} /> */}
+        {/* <Route path="/contact" element={<Contact />} /> */}
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
       </Routes>

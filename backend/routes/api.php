761c2f5252f74baabae1220a05f51c08b344c978
Faycal <faysaloumzil1@gmail.com>
2025-04-14 18:53:03 +0100

    Faycal  resolve problem api

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -4,7 +4,11 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\AuthController;
 use App\Http\Controllers\Admin\ProduitController;
-
+use App\Http\Controllers\Admin\CategoryController;
+use App\Http\Controllers\Admin\CommandeController;
+use App\Http\Controllers\Admin\FactureController;
+use App\Http\Controllers\Admin\ReductionController;
+use App\Http\Controllers\Admin\UserController;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
@@ -14,3 +18,14 @@ Route::post('/login', [AuthController::class, 'login']);
 Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
 
 Route::apiResource('/produits', ProduitController::class);
+
+
+
+// Route::prefix('admin')->group(function () {
+    Route::apiResource('categories', CategoryController::class);
+    Route::apiResource('commandes', CommandeController::class);
+    Route::apiResource('factures', FactureController::class);
+    Route::apiResource('produits', ProduitController::class);
+    Route::apiResource('reductions', ReductionController::class);
+    Route::apiResource('users', UserController::class);
+// });
